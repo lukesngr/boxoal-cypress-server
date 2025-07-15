eb1d7e0bb4c11054361e0f6dbf2722872eb278d8
Luke Sanger <lukesngr@gmail.com>
2025-07-16 09:39:10 +1000

    forgot to readd these tests

--- cypress/e2e/advancedFeatures.cy.js
+++ cypress/e2e/advancedFeatures.cy.js
@@ -9,29 +9,49 @@ describe('advanced features test', () => {
     )
   })
 
-  
+  it('test next goal in line comes when update', () => {
+    cy.intercept('GET', '**/api/getSchedules*').as('getSchedules');
+    cy.visit('http://localhost:3000/myschedules');
+    cy.wait('@getSchedules');
+    cy.get('.openUpdateGoalButton').click();
+    cy.get('.goalCompletedButton').click();
+    cy.get('.updateGoalButton').click();
+    cy.get('.errorAlert').contains('Updated goal!');
+    cy.get('.errorCloseButton').click();
+    cy.get('.goalTitle').contains('next')
+  })
+
+  it('test next goal in line comes when delete', () => {
+    cy.intercept('GET', '**/api/getSchedules*').as('getSchedules');
+    cy.visit('http://localhost:3000/myschedules');
+    cy.wait('@getSchedules');
+    cy.get('.openSkillTree').click();
+    cy.get('.addGoalToTree').click();
+    cy.get('.createGoalTitle').type('next2');
+    cy.get('.createGoalButton').click();
+    cy.get('.errorAlert').contains('Created goal!');
+    cy.get('.errorCloseButton').click();
+    cy.get('.closeGoalTree').click();
+    cy.get('.openUpdateGoalButton').click();
+    cy.get('.deleteGoal').click();
+    cy.get('.errorAlert').contains('Deleted goal!');
+    cy.get('.goalTitle').contains('next2')
+  })
 
   it('test reoccurring', () => {
     cy.intercept('GET', '**/api/getSchedules*').as('getSchedules');
     cy.visit('http://localhost:3000/myschedules');
     cy.wait('@getSchedules');
 
-    let arrayOfSelectors = [];
-    for(let i = 0; i < 7; i++) {
-      let datetime = dayjs().add(2, 'hour').day(i).minute(30).format('HH:mmD/M');
-      arrayOfSelectors.push(`[class="${datetime}TimeboxActionsForm"]`);
-    }
-
     let currentDatetime = dayjs().add(2, 'hour').minute(30).format('H:mmD/M');
     cy.get(`[class="${currentDatetime}TimeboxCreationForm"]`).click();
     cy.get('.createTimeboxTitle').type('test');
+    cy.get('.openReoccurring').click();
     cy.get('.turnReoccuringOn').click();
     cy.get('.createTimebox').click()
     cy.get('.errorAlert').contains('Added timebox!');
-    cy.get('.updateTimeboxButton').click();
-    cy.get('.errorAlert').contains('Updated timebox!');
-    for(let i = 0; i < arrayOfSelectors.length; i++) {
-      cy.get(arrayOfSelectors[i]).contains('test');
-    }
+    cy.get('.errorCloseButton').click();
+    let currentDatetimeDifferentFormatting = dayjs().add(2, 'hour').minute(30).format('HH:mmD/M') //dumb thing in my code i cbf to changed
+    cy.get(`[class="${currentDatetimeDifferentFormatting}TimeboxActionsForm"]`).should('have.length', 7);
   })
 })
